0049e95b662fd21b243a42deaf81b31c7b7c9cc9
Lokesh Dudigollar <lokesh.dudigollar@gmail.com>
2025-05-04 01:19:20 +0530

    first commit for app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request, send_file, session
+from PIL import Image
+import pytesseract
+import pandas as pd
+import os
+import cv2
+import numpy as np
+import re
+from io import BytesIO
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
